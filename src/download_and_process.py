85591c972afbcb6cb188a080785d5c127f0a43df
ZuzannaHeldt <zuzannahel@gmail.com>
2024-12-23 22:53:29 +0100

    Changed download_and_process.py

--- src/download_and_process.py
+++ src/download_and_process.py
@@ -4,29 +4,6 @@ import shutil
 import requests
 from src.utils import get_repo_path
 
-# Lista plików do pobrania
-FILES = [
-    "themes.csv.gz",
-    "colors.csv.gz",
-    "part_categories.csv.gz",
-    "parts.csv.gz",
-    "part_relationships.csv.gz",
-    "elements.csv.gz",
-    "sets.csv.gz",
-    "minifigs.csv.gz",
-    "inventories.csv.gz",
-    "inventory_parts.csv.gz",
-    "inventory_sets.csv.gz",
-    "inventory_minifigs.csv.gz",
-]
-
-BASE_URL = "https://cdn.rebrickable.com/media/downloads/"
-
-# Ścieżki do folderów
-repo_path = get_repo_path()
-RAW_DATA_DIR = repo_path / "data/raw"
-PROCESSED_DATA_DIR = repo_path / "data/processed"
-
 # Funkcja pobierająca pliki do folderu data/raw
 def download_files():
     os.makedirs(RAW_DATA_DIR, exist_ok=True)
